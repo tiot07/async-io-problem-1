a25fdbd13ce7086d9691b8d92c2f0cc76596b110
tiot07 <tiot7155@gmail.com>
2018-12-31 22:57:31 +0900

    同時変更の練習

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const fs = require('fs');
 const fileName = './test.txt';
 for (let count = 0; count < 500; count++) {
-	fs.appendFile(fileName, 'あ', 'utf8');
-	fs.appendFile(fileName, 'い', 'utf8');
-	fs.appendFile(fileName, 'う', 'utf8');
-	fs.appendFile(fileName, 'え', 'utf8');
-	fs.appendFile(fileName, 'お', 'utf8');
-	fs.appendFile(fileName, '\n', 'utf8');
+	fs.appendFile(fileNameSuync, 'あ', 'utf8');
+	fs.appendFile(fileNameSuync, 'い', 'utf8');
+	fs.appendFile(fileNameSuync, 'う', 'utf8');
+	fs.appendFile(fileNameSuync, 'え', 'utf8');
+	fs.appendFile(fileNameSuync, 'お', 'utf8');
+	fs.appendFile(fileNameSuync, '\n', 'utf8');
 }
